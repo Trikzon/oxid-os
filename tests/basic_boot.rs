7897d180c1ec19963dd82fe2869d2185df1c47c8
Trikzon <trikzondev@gmail.com>
2020-12-20 03:45:50 -0500

    Testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::all)]
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
@@ -9,29 +8,18 @@ use libr_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     libr_os::test_panic_handler(info);
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
 
